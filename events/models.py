d1c009b59a85f9137fba7e02797268c0957a603f
ejit0919 <ejit_0919@yahoo.com>
2018-11-07 17:43:57 +0800

    Implemented MyEvents, Create and Uodate Events

--- events/models.py
+++ events/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Event(models.Model):
-    name = models.CharField("Name of event", max_length = 100, default = "")
+    name = models.CharField("Name of event", max_length=100, default="")
     description = models.TextField("Description", max_length=2000, blank=True)
     max_slots = models.IntegerField("Max Slots", blank=True, null=True)
     date_from = models.DateField("Start Date")
@@ -13,5 +13,10 @@ class Event(models.Model):
     venue = models.CharField("Venue", max_length=100)
     date_created = models.DateTimeField("Date Created", auto_now_add=True)
 
+    creator = models.ForeignKey(to="users.Participant", on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("event_detail", args=[str(self.pk)])
